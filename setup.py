4f8196bb533088d82d481fbee67a97fb73707eb9
Stephan Geulette <s.geulette@imio.be>
2016-09-21 18:34:23 +0200

    Set version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.behavior.internalnumber',
-    version='1.0a1',
+    version='0.1',
     description="Configurable internal number plone behavior",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
