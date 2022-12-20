95767dcc444977d15f80e126752a380c6039d835
Stephan Geulette <s.geulette@imio.be>
2022-12-20 10:38:30 +0100

    Removed data transfer info

--- src/collective/behavior/internalnumber/subscribers.py
+++ src/collective/behavior/internalnumber/subscribers.py
@@ -9,9 +9,7 @@ from . import TYPE_CONFIG
 
 
 def object_added(obj, event):
-    """Increments the registry default_number for the type or globally (if one of both configured).
-
-    data transfer: ❓"""
+    """Increments the registry default_number for the type or globally (if one of both configured)."""
     # internal_number is unknown or empty => no need to increment
     if not base_hasattr(obj, 'internal_number') or not obj.internal_number:
         return
