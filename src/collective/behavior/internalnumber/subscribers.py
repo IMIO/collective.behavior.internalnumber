24af777dceca7e2ee077002352587d149391934d
Gauthier Bastien <gauthier.bastien@imio.be>
2023-05-04 12:27:17 +0200

    Fixed call to increment_nb_for in object_added

--- src/collective/behavior/internalnumber/subscribers.py
+++ src/collective/behavior/internalnumber/subscribers.py
@@ -6,4 +6,4 @@ from collective.behavior.internalnumber.browser.settings import increment_nb_for
 
 def object_added(obj, event):
     """Increments the registry default_number for the type or globally (if one of both configured)."""
-    increment_nb_for(obj.portal_type)
+    increment_nb_for(obj)
